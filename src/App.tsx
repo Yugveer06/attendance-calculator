e428c154a8c047c6739dd72e9a3b464480e1075d
Yugveer Singh Wadzatia <yugveersingh06@gmail.com>
2024-08-17 01:46:57 +0530

    removed beams background due to performance reasons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import AttendanceCalculator from "./Pages/AttendanceCalculator";
 import { ThemeProvider } from "./components/theme-provider";
-import { BackgroundBeams } from "./components/ui/background-beams";
 
 function App() {
 	return (
 		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-			<BackgroundBeams />
 			<AttendanceCalculator />
 		</ThemeProvider>
 	);
